e0311384cd55f37f20c4f764db579c50ceb4094c
Fabiana Fornerino <fabianafornerino@gmail.com>
2019-08-14 14:05:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,3 +23,6 @@ var kittens = ["Milo", "Otis", "Garfield"] //resetting array
 function destructivelyRemoveFirstKitten(name){
   return kittens.shift(name);
 }
+var kittens = ["Milo", "Otis", "Garfield"] //resetting array
+
+function 
