384c9e04c00f84d8ac98ca23641182230c7b19e5
Fabiana Fornerino <fabianafornerino@gmail.com>
2019-08-14 13:58:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 // Add your functions and code here
 
 function destructivelyAppendKitten(name){
- return kittens = kittens.push(name);
+ kittens = kittens.push(name);
+ return kittens;
 }
